a07f298770b3a80fd26705a6562c666865ac5d0a
Bret Funk <bret.e.funk@gmail.com>
2017-06-21 16:28:00 -0500

    added new files

--- wiki.rb
+++ wiki.rb
@@ -1,1 +1,16 @@
 require "sinatra"
+
+
+def page_content(title)
+  File.read("pages/#{title}.txt")
+rescue Errno::ENOENT
+  return nil
+end
+
+get "/" do
+  erb :welcome
+end
+
+get "/:title" do
+  page_content(params[:title])
+end
