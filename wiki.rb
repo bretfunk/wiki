2f5f17866798e20c573661ff48b257567adbb5af
Bret Funk <bret.e.funk@gmail.com>
2017-06-21 17:17:18 -0500

    creating forms and new erb templates

--- wiki.rb
+++ wiki.rb
@@ -1,16 +1,27 @@
 require "sinatra"
 
-
 def page_content(title)
   File.read("pages/#{title}.txt")
 rescue Errno::ENOENT
   return nil
 end
 
+def save_content(title, content)
+  File.open("pages/#{title}.txt", "w") do |file|
+    file.print(content)
+  end
+end
+
 get "/" do
   erb :welcome
 end
 
+get "/new" do
+  erb :new
+end
+
 get "/:title" do
-  page_content(params[:title])
+  @title = params["title"]
+  @content = page_content(@title)
+  erb :show
 end
